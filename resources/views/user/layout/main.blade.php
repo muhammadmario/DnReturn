61648b25c9a2ddf060168b6e4da9801d6b18d043
Mario Baron <mariobaron1210@gmail.com>
2023-10-31 02:50:45 +0700

    add navbar

--- resources/views/user/layout/main.blade.php
+++ resources/views/user/layout/main.blade.php
@@ -5,13 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
     @include('user.components.navbar')
-    <main class="container" style="margin-top: 80px; margin-bottom: 80px">
+    <main class="container">
         @yield('content')
     </main>
 
     @include('user.components.footer')
+
+    @stack('scripts')
+    <script>
+        const burger = document.getElementById('burger');
+        const mobileList = document.getElementById('mobileList');
+        
+        burger.addEventListener('click', () => {
+            mobileList.classList.toggle('flex'); // Mengganti 'flex' menjadi 'hidden' atau sebaliknya
+            mobileList.classList.toggle('hidden');
+        });
+    </script>
 </body>
 </html>
