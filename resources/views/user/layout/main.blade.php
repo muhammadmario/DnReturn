bec35c210aa02490c668039f61f03d7ca99bb5a7
Mario Baron <mariobaron1210@gmail.com>
2023-10-31 00:56:02 +0700

    add setup folder

--- resources/views/user/layout/main.blade.php
+++ resources/views/user/layout/main.blade.php
@@ -5,11 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    @vite('resources/css/app.css')
 </head>
 <body>
-    <h1 class="text-3xl font-bold underline text-green-500">
-        
-    </h1>
+    @include('user.components.navbar')
+    <main class="container" style="margin-top: 80px; margin-bottom: 80px">
+        @yield('content')
+    </main>
+
+    @include('user.components.footer')
 </body>
 </html>
---
+++ resources/views/user/pages/home.blade.php
@@ -0,0 +1,11 @@
+@extends('user.layout.main')
+
+@section('title')
+<title>DnReturn</title>
+@endsection
+
+@section('content')
+<div>
+    cek
+</div>
+@endsection
