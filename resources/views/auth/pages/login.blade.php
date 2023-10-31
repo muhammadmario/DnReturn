4c7aa368083e76222eac4c80de7c828ea89208ee
Mario Baron <mariobaron1210@gmail.com>
2023-10-31 17:05:03 +0700

    fixing ui

--- resources/views/auth/pages/login.blade.php
+++ resources/views/auth/pages/login.blade.php
@@ -5,25 +5,23 @@
 @endsection
 
 @section('content')
-<div class="max-w-xl p-5 mx-auto min-h-screen">
-    <form class="mt-36">
+<div class="max-w-xl p-5 mx-auto min-h-[80vh]">
+    <form class="mt-28 bg-white text-black p-10 rounded-lg">
         <div class="mb-6">
-            <label for="email" class="block mb-2 text-sm font-medium">Your email</label>
-            <input type="email" id="email" class=" border   text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-40 focus:ring-blue-500 focus:border-blue-500" placeholder="name@flowbite.com" required>
+            <label for="username" class="block mb-2 text-sm font-medium ">Username</label>
+            <input type="text" id="username" class="border text-sm rounded-lg block w-full p-2.5 border-gray-400 placeholder-gray-40 focus:ring-blue-500 focus:border-blue-500" placeholder="Username" required>
         </div>
         <div class="mb-6 relative">
-            <label for="password" class="block mb-2 text-sm font-medium">Your password</label>
-            <input type="password" id="password" class=" border   text-sm rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-40 focus:ring-blue-500 focus:border-blue-500" required>
+            <label for="password" class="block mb-2 text-sm font-medium">Password</label>
+            <input type="password" id="password" class=" border text-sm rounded-lg block w-full p-2.5 border-gray-400 placeholder-gray-40 focus:ring-blue-500 focus:border-blue-500" placeholder="Password" required>
             <i class="absolute top-10 right-3 cursor-pointer fas fa-eye" id="togglePassword"></i>
         </div>
         <div class="flex items-end justify-end mb-6">
-            <a href="/forgot-password" class="text-sm font-medium text-end">Forgot your password?</a>
+            <a href="/forgot-password" class="text-sm font-medium text-end text-red-500">Forgot your password?</a>
         </div>
-
         <div>
-            <button type="submit" class="text-white w-full bg-red-800 hover:bg-red-900 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Submit</button>
+            <button type="submit" class="text-white w-full bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign In</button>
         </div>
-        
     </form>
 </div>
 
